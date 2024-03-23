17840fd4427599ff825a8b9aaa75d23d10dd64c3
Igor Tarlinskiy <itarlinskiy@gmail.com>
2024-03-23 22:21:23 +0300

    Add template for loading processor and converting dicts to dataset

--- justatom/processing/mask.py
+++ justatom/processing/mask.py
@@ -1,1 +1,10 @@
+import abc
+
 GRANTED_PROCESSOR_NAMES = ["PFBERTProcessor", "E5SMALLProcessor", "E5Processor", "E5LARGEProcessor"]
+
+
+class IProcessor(abc.ABC):
+
+    @abc.abstractmethod
+    def dataset_from_dicts(self, dicts):
+        pass
