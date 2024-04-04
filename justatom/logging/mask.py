4270d519252e988fb2cba19a902324ffd745220d
Igor Tarlinskiy <itarlinskiy@gmail.com>
2024-04-04 05:50:42 +0300

    Integrate wandb template logging

---
+++ justatom/logging/mask.py
@@ -0,0 +1,31 @@
+import abc
+from loguru import logger
+from typing import Optional
+from justatom.etc.visual import WELCOME_MSG
+
+
+class ILogger(abc.ABC):
+    """
+    Base class for tracking experiments.
+
+    This class can be extended to implement custom logging backends like MLFlow, Tensorboard, or WANDB.
+    """
+
+    disable_logging = False
+
+    def __init__(self, log_batch_metrics: bool, log_epoch_metrics: bool, tracking_uri: Optional[bool] = False, **kwargs):
+        self.tracking_uri = tracking_uri
+        self.log_batch_metrics = log_batch_metrics
+        self.log_epoch_metrics = log_epoch_metrics
+        logger.success(WELCOME_MSG)
+
+    def log_metrics(self, metrics, step, **kwargs):
+        raise NotImplementedError()
+
+    @classmethod
+    def log_artifacts(cls, self):
+        raise NotImplementedError()
+
+    @classmethod
+    def log_params(self, params, **kwars):
+        raise NotImplementedError()
