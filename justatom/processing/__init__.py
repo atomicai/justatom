d8837fe4f139d3665ee7fcad8eb1beec0889e2f3
Igor Tarlinskiy <itarlinskiy@gmail.com>
2024-04-08 19:15:57 +0300

    Integrate [IProcessor] and [ITokenizer] and [igniset] for converting raw data to PyTorch dataset

---
+++ justatom/processing/__init__.py
@@ -0,0 +1,6 @@
+from justatom.processing.silo import igniset
+from justatom.processing.tokenizer import ITokenizer
+from justatom.processing.mask import IProcessor
+
+
+__all__ = ["igniset", "ITokenizer", "IProcessor"]
