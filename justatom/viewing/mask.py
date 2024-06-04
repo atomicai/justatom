e088cde5d4d0dfd9ede985b09eb90967e8a534af
Igor Tarlinskiy <itarlinskiy@gmail.com>
2024-06-04 03:55:13 +0300

    fix

---
+++ justatom/viewing/mask.py
@@ -0,0 +1,12 @@
+import abc
+import polars as pl
+
+
+class IChart(abc.ABC):
+
+    @abc.abstractmethod
+    def view(self, data: pl.DataFrame, **props):
+        pass
+
+    def save(self, filename, ppi=200):
+        self.chart.save(filename, ppi=ppi)
