181903b8108f1ac6e952bd17f330d63c12442b4f
Igor Tarlinskiy <itarlinskiy@gmail.com>
2024-07-08 17:45:09 +0300

    Add interface template for Evaluation pipeline

--- justatom/running/evaluator.py
+++ justatom/running/evaluator.py
@@ -1,5 +1,10 @@
 from typing import Callable, List, Union
 
+import torch
+from more_itertools import chunked
+from tqdm.autonotebook import tqdm
+
+from justatom.modeling.metrics import IAdditiveMetric
 from justatom.running.mask import IEvaluatorRunner, IRetrieverRunner
 
 
@@ -7,12 +12,23 @@ class EvaluatorRunner(IEvaluatorRunner):
 
     def __init__(self, ir: IRetrieverRunner):
         super().__init__(ir=ir)
+        self.metrics = dict()
 
+    @torch.no_grad()
     def evaluate_topk(
         self,
         queries: Union[str, List[str]],
-        dex_name: str,
         metrics: List[Union[str, Callable]],
         top_k: int = 5,
+        batch_size: int = 10,
     ):
-        pass
+        monitor = dict()
+        for batch_queries in tqdm(chunked(queries, n=batch_size)):
+            res_topk = self.ir.retrieve_topk()
+            labels = [
+                c.meta["labels"] for c in res_topk
+            ]  # num_questions_doc1 + num_questions_doc2 + ... + num_questions_dock
+            raise NotImplementedError()
+
+
+__all__ = ["EvaluatorRunner"]
