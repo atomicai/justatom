21e003056ecaa239cba1f5b3f5a7ac5732aba202
Igor Tarlinskiy <itarlinskiy@gmail.com>
2024-07-08 22:10:48 +0300

    M1 model fix

--- justatom/running/m1.py
+++ justatom/running/m1.py
@@ -1,24 +1,29 @@
-from justatom.running.mask import IMODELRunner
 from pathlib import Path
+from typing import List, Optional, Union
+
 import simplejson as json
-from loguru import logger
-from typing import Union, List, Optional
-from justatom.modeling.mask import ILanguageModel
-from justatom.modeling.mask import IHead
 import torch
+from loguru import logger
+
 from justatom.modeling.div import loss_per_head_sum
+from justatom.modeling.mask import IHead, ILanguageModel
+from justatom.running.mask import IMODELRunner
 
 
 class M1LMRunner(IMODELRunner, torch.nn.Module):
 
-    def __init__(self, model: ILanguageModel, prediction_heads: List[IHead], device="cpu"):
+    def __init__(
+        self, model: ILanguageModel, prediction_heads: List[IHead], device="cpu"
+    ):
         super(M1LMRunner, self).__init__()
         self.model = model
         self.prediction_heads = prediction_heads or []
         self.device = device
         self.dropout = torch.nn.Dropout(0.1)
         self.loss_aggregation_fn = loss_per_head_sum
-        self.config = dict(prediction_heads=[hi.generate_config() for hi in prediction_heads or []])
+        self.config = dict(
+            prediction_heads=[hi.generate_config() for hi in prediction_heads or []]
+        )
 
     def to(self, device):
         logger.info(f"Moving to device {str(device)}")
@@ -32,7 +37,7 @@ class M1LMRunner(IMODELRunner, torch.nn.Module):
     @classmethod
     def load(cls, data_dir: Union[Path, str], config=None, **props):
         # model_config.json supposed to be present in directory
-        _model_config = Path(data_dir) / "model_config.json"
+        _model_config = Path(data_dir) / "runner_config.json"
         assert _model_config.exists(), "The model file is not found for `M1Runner`"
         model = ILanguageModel.load(Path(data_dir))
         if config is None:
@@ -55,17 +60,25 @@ class M1LMRunner(IMODELRunner, torch.nn.Module):
                  have length n_pred_heads, batch_size.
         """
         all_losses = []
-        for head, logits_for_one_head, labels_for_one_head in zip(self.prediction_heads, logits, labels):
+        for head, logits_for_one_head, labels_for_one_head in zip(
+            self.prediction_heads, logits, labels
+        ):
             # check if PredictionHead connected to Processor
             assert hasattr(head, "label_tensor_name"), (
                 f"Label_tensor_names are missing inside the {head.task_name} Prediction Head. Did you connect the model"
                 " with the processor through either 'model.connect_heads_with_processor(processor.tasks)'"
                 " or by passing the processor to the Adaptive Model?"
             )
-            all_losses.append(head.logits_to_loss(logits=logits_for_one_head, labels=labels_for_one_head))
+            all_losses.append(
+                head.logits_to_loss(
+                    logits=logits_for_one_head, labels=labels_for_one_head
+                )
+            )
         return all_losses
 
-    def logits_to_loss(self, logits: torch.Tensor, global_step: Optional[int] = None, **kwargs):
+    def logits_to_loss(
+        self, logits: torch.Tensor, global_step: Optional[int] = None, **kwargs
+    ):
         """
         Get losses from all prediction heads & reduce to single loss *per sample*.
 
@@ -78,7 +91,9 @@ class M1LMRunner(IMODELRunner, torch.nn.Module):
         all_losses = self.logits_to_loss_per_head(logits, **kwargs)
         # This aggregates the loss per sample across multiple prediction heads
         # Default is sum(), but you can configure any fn that takes [Tensor, Tensor ...] and returns [Tensor]
-        loss = self.loss_aggregation_fn(all_losses, global_step=global_step, batch=kwargs)
+        loss = self.loss_aggregation_fn(
+            all_losses, global_step=global_step, batch=kwargs
+        )
         return loss
 
     def prepare_labels(self, labels):
