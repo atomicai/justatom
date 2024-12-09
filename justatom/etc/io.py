cb79eb9eb264ac02a56d99fcedfa6e1d647f52d0
Igor Tarlinskiy <itarlinskiy@gmail.com>
2024-12-09 17:14:07 +0500

    Snapshot for periodically saving result during remote synthetic data generation

--- justatom/etc/io.py
+++ justatom/etc/io.py
@@ -1,3 +1,6 @@
+import simplejson as json
+
+
 def delete_folder(pth):
     for sub in pth.iterdir():
         if sub.is_dir():
@@ -7,4 +10,28 @@ def delete_folder(pth):
     pth.rmdir()
 
 
-__all__ = ["delete_folder"]
+def io_snapshot(data, where=None, snapshot_number: str = "0", snapshot_prefix: str = None, snapshot_suffix: str = None):
+    import os
+    from pathlib import Path
+
+    from loguru import logger
+
+    where = Path(os.getcwd()) if not where else Path(where)
+    snapshot_prefix = "" if snapshot_prefix is None else snapshot_prefix
+    snapshot_suffix = "" if snapshot_suffix is None else snapshot_suffix
+    filename = f"{snapshot_prefix}{str(snapshot_number)}{snapshot_suffix}.json"
+    where_path = where / filename
+    where.mkdir(parents=True, exist_ok=True)
+    is_ok: bool = None
+    try:
+        with open(str(where_path), "w+") as fout:
+            json.dump(data, fout, ensure_ascii=False)
+    except:  # noqa
+        logger.error(f"The data coming {data} is not JSON compliant")
+        is_ok = False
+    else:
+        is_ok = True
+    return is_ok
+
+
+__all__ = ["delete_folder", "io_snapshot"]
