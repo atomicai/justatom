617335d53079580431943fdfe917a1ab6e301497
Igor Tarlinskiy <itarlinskiy@gmail.com>
2024-12-10 03:14:54 +0500

    Add REPHRASEPromptRunner and fix some prompt mistakes

--- justatom/running/prompt.py
+++ justatom/running/prompt.py
@@ -7,17 +7,19 @@ from justatom.running.mask import IPromptRunner
 
 
 class KEYPromptRunner(IPromptRunner):
-    def __init__(self, system_prompt: str):
+    def __init__(self, system_prompt: str, source_language: str = "русском", **props):
         super().__init__(system_prompt=system_prompt.strip())
+        self.source_language = source_language
 
-    def _prepare(self, content: str, title: str, source_language: str = "русском", **props):
+    def _prepare(self, content: str, title: str, source_language: str | None = None, **props):
+        source_language = source_language or self.source_language
         prompt = f"""
         Обрати внимание, что ключевые слова или фразы должны быть подстрокой параграфа и состоять из НЕ более двух, максимум трех слов.\n
-        Каждая фраза или ключевое слово должны объясняться на {source_language} языке и иметь краткое, но емкое объяснение в зависимости от контекста, в котором они употреблены. \n
+        Каждая фраза или ключевое слово должны иметь краткое, но емкое объяснение на {source_language} языке в зависимости от контекста, в котором они употреблены. \n
         Параграф из вселенной \"{title}\":\n{content}\n\n
-        Выдай ответ в виде  json в формате: {{"keywords_or_phrases": [{{"keyword_or_phrase": <Выделенная тобою фраза>, "explanation": <Объяснение на {source_language} языке для ребенка в соответствии с контекстом, в котором употреблена ключевая фраза или слово}}]}}.\n
+        Выдай ответ в виде  json в формате: {{"keywords_or_phrases": [{{"keyword_or_phrase": <Выделенная тобою фраза>, "explanation": <Объяснение на {source_language} языке для ребенка в соответствии с контекстом, в котором употреблена ключевая фраза или слово>}}]}}.\n
         Выдай только json.
-        """.strip()
+        """.strip()  # noqa
         return prompt
 
     def finalize(self, content: str, raw_response: str, as_json_string: bool = False, **props):
@@ -44,15 +46,35 @@ class KEYPromptRunner(IPromptRunner):
 
 
 class TRLSPromptRunner(IPromptRunner):
-    def __init__(self, system_prompt: str):
+    def __init__(self, system_prompt: str, source_language: str | None = None, **props):
         super().__init__(system_prompt=system_prompt.strip())
+        self.source_language = source_language
 
-    def _prepare(self, content: str, title: str, **props):
-        prompt = f"""\n
+    def _prepare(self, content: str, title: str, source_language: str | None = None, **props):
+        source_language = source_language or self.source_language
+        prompt = f"""
         Параграф из вселенной \"{title}\":\n{content}\n\n
-        Выдай ответ в виде  json в формате: {{"translation": "<Твой перевод, учитывающий контекст параграфа из вселенной {title}>}}"
+        Выдай ответ в виде  json в формате: {{"translation": "<Твой перевод на {source_language} языке, учитывающий контекст параграфа из вселенной {title}>}}"
         Выдай только json.
-        """.strip()
+        """.strip()  # noqa
+        return prompt
+
+    def finalize(self, content: str, raw_response: str, as_json_string: bool = False, **props):
+        if as_json_string:
+            return json_repair.loads(raw_response)
+        return raw_response
+
+
+class REPHRASEPromptRunner(IPromptRunner):
+    def __init__(self, system_prompt: str, **props):
+        super().__init__(system_prompt=system_prompt.stirp())
+
+    def _prepare(self, content, title: str, **props):
+        prompt = f"""
+        Сделай несколько (две или более) парафраз из исходного параграфа или вопроса.\n
+        Параграф (вопрос) из вселенной\"{title}\":\n{content}\n\n
+        Выдай ответ в виде json в формате: {{"rephrase_phrases": "[{{"phrase": <Твой пересказ, учитывающий контекст параграфа или вопроса из вселенной {title}>}}"}}
+        """.strip()  # noqa
         return prompt
 
     def finalize(self, content: str, raw_response: str, as_json_string: bool = False, **props):
