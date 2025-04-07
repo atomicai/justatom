914f0e1d9f50ec44464ec78f3d738cfb5f049ec1
Igor Tarlinskiy <itarlinskiy@gmail.com>
2025-04-07 12:55:47 +0500

    Patcher update  (still sync not async)

--- justatom/running/patcher.py
+++ justatom/running/patcher.py
@@ -4,37 +4,45 @@ from tqdm import tqdm
 
 from justatom.running.mask import IPatcherRunner
 from justatom.storing.weaviate import Finder as WeaviateApi
+from justatom.storing.weaviate import WeaviateDocStore
+from justatom.tooling.stl import AsyncConstructor
 
 
-class PatcherRunner(IPatcherRunner):
-    def __init__(self, collection_name, new_collection_name):
+class PatcherRunner(IPatcherRunner, AsyncConstructor):
+    async def __init__(self, collection_name, new_collection_name):
+        super().__init__()
         self.collection_name = collection_name
         self.new_collection_name = new_collection_name
-        self.st_prev = WeaviateApi.find(collection_name)
-        self.st_next = WeaviateApi.find(new_collection_name)
+        self.current_doc_store: WeaviateDocStore = await WeaviateApi.find(collection_name)
+        self.next_doc_store: WeaviateDocStore = await WeaviateApi.find(new_collection_name)
 
     async def patch(self, batch_size: int = 128, **kwargs) -> tuple:
-        it_collection_data = self.st_prev.get_all_documents(include_vector=True)
+        it_collection_data = await self.current_doc_store.get_all_documents(include_vector=True)
+        total_written_docs, status = 0, 200
         for batch_idx, js_batch_we_docs in tqdm(enumerate(chunked(it_collection_data, n=batch_size))):
-            js_batch_docs = [self.st_prev._to_document(js_we_doc) for js_we_doc in js_batch_we_docs]
-            logger.info(f"batch_idx=[{str(batch_idx)}]")
+            js_batch_docs = [self.current_doc_store._to_document(js_we_doc) for js_we_doc in js_batch_we_docs]
             try:
-                self.st_next.write_documents(js_batch_docs)
-            except:
-                return {
-                    f"COUNT[{self.collection_name}]": self.st_prev.count_documents(),
-                    f"COUNT[{self.new_collection_name}]": self.st_next.count_documents(),
-                }, 404
+                batch_written_docs = await self.next_doc_store.write_documents(js_batch_docs)
+            except:  # noqa
+                logger.info(f"{self.__class__.__name__} | `batch_idx`=[{batch_idx}]")
+                status = 404
             else:  # TODO:
-                pass
-        is_removal_ok: bool = self.st_prev.delete_all_documents()
+                total_written_docs += batch_written_docs
+                logger.info(
+                    f"""
+                            {self.__class__.__name__} | count({self.new_collection_name})=[{total_written_docs}]
+                    """
+                )
+        is_removal_ok: bool = await self.current_doc_store.delete_all_documents()
         if is_removal_ok:
-            msg = f"Removal on `collection_name`=[{self.collection_name}] is successfull"
+            msg = f"{self.__class__.__name__} | Removal on `collection_name`=[{self.collection_name}]  "
             logger.info(msg)
         else:
-            msg = f"Remove on `collection_name`=[{self.collection_name}] is wrong"
+            msg, status = f"Remove on `collection_name`=[{self.collection_name}] failed", 404
             logger.error(msg)
+        n_count_docs_current = await self.current_doc_store.count_documents()
+        n_count_docs_next = await self.next_doc_store.count_documents()
         return {
-            f"COUNT[{self.collection_name}]": self.st_prev.count_documents(),
-            f"COUNT[{self.new_collection_name}]": self.st_next.count_documents(),
-        }, 200
+            f"count[{self.collection_name}]": n_count_docs_current,
+            f"count[{self.new_collection_name}]": n_count_docs_next,
+        }, status
