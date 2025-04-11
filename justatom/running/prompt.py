3ce4d08c307f292ea62f62ba85bcce29fdf39d42
Igor Tarlinskiy <itarlinskiy@gmail.com>
2025-04-12 01:38:51 +0500

    Prompt update

--- justatom/running/prompt.py
+++ justatom/running/prompt.py
@@ -209,7 +209,6 @@ class QUERIESWithSourcesPromptRunner(IPromptRunner):
         - \"response\" is an answer that can be found directly in the document text.
         - \"relevance\" is an array indicating how crucial each paragraph is to the question, on a scale from 0 to 10.
         \n
-        > Important: The sum of the relevance scores for each question must not exceed 10 in total.
         Please respond only with the JSON.
         """.strip()  # noqa
         return prompt
@@ -257,13 +256,72 @@ class RESPONSEWithConfirmPromptRunner(IPromptRunner):
         \n{content}\n
         Your output must be in JSON format and include the following structure:
         - "answer": [{{
-            "question": \"...\", // The question, exactly as given in as input
             "response": \"...\", // Your generated answer (or comment, if info is missing) in {source_language}
             "is_context_present": \"...\" // Boolean indicating if the necessary info is in the paragraph
         }}, ...]\n
         Where:
         - \"question\" is the exact question from the input.
-        - \"response\" is your answer in {source_language} language stated naturally without referencing \"the paragraph\n".
+        - \"response\" is your answer in {source_language} language stated naturally without referencing \"the paragraph\".
+        - \"is_context_present\" is either true or false, indicating whether the paragraph contains the required information.\n
+        \n
+        **Respond only with the JSON.** Avoid any meta-explanations or references to the prompt or paragraph.
+        """  # noqa
+
+        return prompt
+
+    def finalize(self, content: str, raw_response: str, as_json_string: bool = False, **props):
+        if as_json_string:
+            return json_repair.loads(raw_response)
+        return raw_response
+
+
+class RESPONSEWithConfirmAndSourcesPromptRunner(IPromptRunner):
+    _system_prompt = f"""
+    You are a highly capable language assistant with remarkable skillset on the following:
+    - History and mechanics of computer games.
+    - Well-versed in many films.
+    - Skilled at providing user support and guidance for complex systems (e.g. user portals, 
+      databases, or other technical domains).
+    - Scientific facts and general historical facts
+    """  # noqa
+
+    def __init__(self, source_language: str | None = None, title: str | None = None, system_prompt: str = None, **props):
+        super().__init__(system_prompt=system_prompt.strip() if system_prompt is not None else self._system_prompt.strip())
+        self.title = title
+        self.source_language = source_language
+
+    def _prepare(self, query: str, content: str, source_language: str | None = None, title: str | None = None, **props):
+        source_language = source_language or self.source_language
+        title = title or self.title
+        prompt = f"""
+        You are given a query and a paragraphs containing relevant information.\n
+        Each paragraph is numbered in sequence, starting from 0. For response you generate, specify how strongly it relates to each paragraph on a scale from 0 to 10:
+            - 0 means it is not related at all.
+            - 10 means the question cannot be answered without that paragraph.\n**However**, when generating your answer:\n
+        1. **Do not** explicitly reference or cite the paragraph text (e.g., do not say "according to the provided paragraph" or "based on the text"). \n
+        2. Write your response **as if you have known the facts all along**. \n
+        3. Use a natural style of communication in {source_language}.\n
+        4. If the paragraphs does **not** contain any information to answer the query, respond with a brief comment explaining the lack of information.\n
+        Generate an answer in {source_language} based on the information provided:\n
+        -  \"query\" represents the user's question, belonging to the universe (theme) \"{title}\".\n
+        -  \"Paragraph <num>\" refers to the text at position <num> from which you must derive your answer and set the relevance score. Remember, each paragraphs starts with \"Paragraph <num>\" where <num> indicates the index of the paragraph\n
+        > Important: If the required information is not found in the paragraph, do not invent an answer. Instead:\n
+        - Set "is_context_present" to false.\n
+        - Replace the answer with a brief comment explaining why the information is missing.\n
+        When relevant, refer to the universe (theme) \"{title}\" if the question is broad enough or might be misunderstood without it. Avoid referencing it otherwise. However, if the question concerns a specific book, game, movie, or unique section, be explicit.\n
+        Your answer must be strictly in {source_language}.\n
+        Below are the query and paragraphs related to the topic \"{title}\":\n
+        \n{query}\n
+        \n{content}\n
+        Your output must be in JSON format and include the following structure:
+        - "answer": [{{
+            "response": \"...\", // Your generated answer (or comment, if info is missing) in {source_language}
+            "relevance": [ ... ] // Array of relevance scores (0–10) for each paragraph
+            "is_context_present": \"...\" // Boolean indicating if the necessary info is in the paragraph
+        }}, ...]\n
+        Where:
+        - \"response\" is your answer in {source_language} language stated naturally without referencing \"the paragraph\"".
+        - \"relevance\" is an array indicating how crucial each paragraph is to the question, on a scale from 0 to 10.
         - \"is_context_present\" is either true or false, indicating whether the paragraph contains the required information.\n
         \n
         **Respond only with the JSON.** Avoid any meta-explanations or references to the prompt or paragraph.
